d2c365761c9da07b192377da07a5d8aae6e64574
brkd <berkayd1812@gmail.com>
2025-12-20 14:46:12 +0300

    bugfix: moved dependencies so nothing loads at import time

--- src/api/app.py
+++ src/api/app.py
@@ -2,40 +2,17 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from ingestion.loader import PDFLoader
-from ingestion.cleaner import Cleaner
-from ingestion.chunker import Chunker
-
-from vectorstore.vectorstore import VectorStoreBuilder
-from rag.llm import LLMInterface
-from rag.pipeline import RAGPipeline
-
-from config.config import AppConfig
-
-app_config = AppConfig()
-
-loader = PDFLoader(app_config.PDF_LOCATION)
-cleaner = Cleaner()
-chunker = Chunker(chunk_size=app_config.CHUNK_SIZE, chunk_overlap=app_config.CHUNK_OVERLAP)
-
-vectorstore = VectorStoreBuilder(
-    collection_name=app_config.CHROMA_COLLECTION,
-    host=app_config.CHROMA_HOST,
-    port=app_config.CHROMA_PORT,
-    embedding_model=app_config.EMBEDDING_MODEL,
-)
-
-llm = LLMInterface(
-    model=app_config.LLM_MODEL,
-    inference_server_url=app_config.INFERENCE_SERVER_URL,
-    temperature=app_config.LLM_TEMPERATURE,
-    max_tokens=app_config.LLM_MAX_TOKENS
-)
+from api.app_services import get_ingestion_components, get_config, get_vectorstore
 
 
 # Async context manager for startup operations
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    config = get_config()
+
+    loader, cleaner, chunker = get_ingestion_components(config)
+    vectorstore = get_vectorstore(config)
+
     documents = loader.load()
     cleaned_documents = cleaner.clean(documents)
     chunks = chunker.chunk(cleaned_documents)
@@ -54,4 +31,6 @@ app.include_router(api_router)
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run("app:app", host=app_config.API_HOST, port=app_config.API_PORT, reload=False)
+    config = get_config()
+
+    uvicorn.run("app:app", host=config.API_HOST, port=config.API_PORT, reload=False)
